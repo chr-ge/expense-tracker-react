ef086eeeb36f4e7f1bb9af13855f01c179071252
chr-ge <ace26@protonmail.com>
2020-05-21 20:10:12 -0400

    created components

---
+++ src/components/Balance.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import CountUp from 'react-countup';
+
+const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(t => t.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1 className="balance">$
+                <CountUp start={0} end={total} duration={2.00} separator="," />
+            </h1>
+        </>
+    )
+}
+
+export default Balance
